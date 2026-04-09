f98c0af3624127684383887bec0121f2b67a8e00
hamza lazaar <lazaarhamza39@gmail.com>
2026-04-10 00:59:39 +0200

    feat: Version finale fonctionnelle avec JDK 17

---
+++ src/main/java/org/example/Launcher.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Cette ligne appelle ton Main actuel sans passer par les vérifications de modules JavaFX
+        Main.main(args);
+    }
+}
